49e7af525fd0429e33aedeed891cfdac38f4732e
William Brown <1208343948@qq.com>
2024-03-15 13:15:25 +0800

    feat:封装前台数据接口

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,5 @@
-import type { BannerItem } from '@/types/home'
-import { http } from '@/utils/http'
+import type { BannerItem } from '@/types/home';
+import { http } from '@/utils/http';
 
 /**
  * 首页 广告区域 小程序
@@ -13,5 +13,16 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
-  })
-}
+  });
+};
+
+/**
+ *
+ * 首页 前台分类 小程序
+ */
+export const getHomeCategoryAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/home/category/mutli',
+  });
+};
