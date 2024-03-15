589c296bdcc6c1906c2180ec0b1a86bedd9e8f30
William Brown <1208343948@qq.com>
2024-03-15 13:14:35 +0800

    feat:前台分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,41 @@
 <script setup lang="ts">
-import { getHomeBannerAPI } from '@/services/home'
-import CustomNavbar from './components/CustomNavbar.vue'
-import { onLoad } from '@dcloudio/uni-app'
-import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import { ref } from 'vue';
 
-const bannerList = ref<BannerItem[]>([])
+import CustomNavbar from './components/CustomNavbar.vue';
+import CategoryPanel from './components/CategoryPanel.vue';
+import { onLoad } from '@dcloudio/uni-app';
+import type { BannerItem } from '@/types/home';
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home';
+
+const bannerList = ref<BannerItem[]>([]);
 const getHomeBannerData = async () => {
-  const res = await getHomeBannerAPI()
-  bannerList.value = res.result
-}
+  const res = await getHomeBannerAPI();
+  bannerList.value = res.result;
+};
+
+// 获取前台分类数据
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI();
+  console.log(res);
+};
 
 onLoad(() => {
-  getHomeBannerData()
-})
+  getHomeBannerData();
+  getHomeCategoryData();
+});
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
