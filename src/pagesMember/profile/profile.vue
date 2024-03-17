f511da358b687d032fe88c982bedf3ee81940dc4
William Brown <1208343948@qq.com>
2024-03-17 17:25:30 +0800

    feat:个人信息 - 城市修改

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -68,6 +68,14 @@ const onBirthdayChange: UniHelper.DatePickerOnChange = (ev) => {
   profile.value.birthday = ev.detail.value;
 };
 
+// 修改城市
+let fullLocationCode: [string, string, string] = ['', '', ''];
+const onFullLocationChange: UniHelper.RegionPickerOnChange = (ev) => {
+  // 修改前端界面
+  profile.value.fullLocation = ev.detail.value.join(' ');
+  fullLocationCode = ev.detail.code!;
+};
+
 // 点击保存 提交表单
 const onSubmit = async () => {
   const { nickname, gender, birthday } = profile.value;
@@ -75,6 +83,9 @@ const onSubmit = async () => {
     nickname: nickname,
     gender: gender,
     birthday: birthday,
+    provinceCode: fullLocationCode[0],
+    cityCode: fullLocationCode[1],
+    countyCode: fullLocationCode[2],
   });
   memberStore.profile!.nickname = res.result.nickname;
   setTimeout(() => {
@@ -142,7 +153,12 @@ const onSubmit = async () => {
         </view>
         <view class="form-item">
           <text class="label">城市</text>
-          <picker class="picker" mode="region" :value="profile?.fullLocation?.split(' ')">
+          <picker
+            @change="onFullLocationChange"
+            class="picker"
+            mode="region"
+            :value="profile?.fullLocation?.split(' ')"
+          >
             <view v-if="profile?.fullLocation">{{ profile?.fullLocation }}</view>
             <view class="placeholder" v-else>请选择城市</view>
           </picker>
